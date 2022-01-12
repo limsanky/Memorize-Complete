9522eb002305fc8d7c1ddd4887fd976502769a6d
Sankarshana V <61644684+limsanky@users.noreply.github.com>
2022-01-12 13:39:44 +0900

    Initial Commit

---
+++ Memorize/MemorizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemorizeApp.swift
+//  Memorize
+//
+//  Created by Sankarshana V on 2022/01/12.
+//
+
+import SwiftUI
+
+@main
+struct MemorizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
